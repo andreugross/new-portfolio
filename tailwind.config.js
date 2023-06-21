8c245ffd15cde8094c6ac5d548a2cd8370feb399
André Gross <andgross@gmail.com>
2023-06-21 16:10:54 -0300

    feat: adding fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,10 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
+        mono: ['var(--font-roboto-mono)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
       },
     },
   },
